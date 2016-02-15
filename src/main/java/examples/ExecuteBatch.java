2f6340b88d8355252b8168f487e1fe8f354c1e5e
Isaac Liao <isaac.liao@ubitus.net>
2016-02-15 15:50:25 +0800

    add execute batch example

---
+++ src/main/java/examples/ExecuteBatch.java
@@ -0,0 +1,58 @@
+package examples;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class ExecuteBatch {
+
+    public static void main(String[] params) throws IOException, InterruptedException {
+        Process p = startProcess("ping", "127.0.0.1");
+        p.waitFor();
+        System.out.println(p.exitValue());
+    }
+    
+    private static Process startProcess(String... commands) throws IOException {
+        ProcessBuilder builder = new ProcessBuilder(commands);
+        builder.redirectErrorStream(true);
+        final Process process = builder.start();
+        // process will hang when start process without read console and error input stream
+        readConsoleInputStream(process);
+        readErrorStream(process);
+        return process;
+    }
+
+    private static void readConsoleInputStream(final Process process) {
+        new Thread(){
+            public void run() {
+                try (InputStream in = process.getInputStream()) {
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                    String line = null;
+                    while ((line = reader.readLine()) != null) {
+                        System.out.println(line);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+    }
+    
+    private static void readErrorStream(final Process process) {
+        new Thread(){
+            public void run() {
+                try (InputStream in = process.getErrorStream()) {
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                    String line = null;
+                    while ((line = reader.readLine()) != null) {
+                        System.out.println(line);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+    }
+    
+}
