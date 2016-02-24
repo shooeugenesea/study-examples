1694bc20a2549e756309d0a69a671ba04d6beb1f
Isaac Liao <isaac.liao@ubitus.net>
2016-02-24 15:52:26 +0800

    complete schema and data CRUD

--- src/main/java/examples/hector/Thrift.java
+++ src/main/java/examples/hector/Thrift.java
@@ -1,8 +1,7 @@
 package examples.hector;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -19,6 +18,7 @@ import me.prettyprint.hector.api.Keyspace;
 import me.prettyprint.hector.api.ddl.ColumnFamilyDefinition;
 import me.prettyprint.hector.api.ddl.KeyspaceDefinition;
 import me.prettyprint.hector.api.factory.HFactory;
+import me.prettyprint.hector.api.mutation.Mutator;
 
 /**
  * https://hector-client.github.io/hector/build/html/content/getting_started.html
@@ -50,12 +50,57 @@ public class Thrift {
         deleteRow(cluster, ksName, cfName, rowKey);
         System.out.println("delete row:" + rowKey);
 
+        List<String> rowKeys = createRows(cluster, ksName, cfName);
+        System.out.println("create rows:" + rowKeys);
+        
+        Map<String, Map<String,String>> readRows = readRows(cluster, ksName, cfName, rowKeys);
+        for ( Map.Entry<String, Map<String,String>> entry: readRows.entrySet() ) {
+            String key = entry.getKey();
+            Map<String,String> row = entry.getValue();
+            System.out.println("read rows. rowKey:" + key + ", row:" + row);
+        }
+
+        deleteRows(cluster, ksName, cfName, rowKeys);
+        System.out.println("delete rows:" + rowKeys);
+        
         cluster.dropColumnFamily(ksName, cfName);
         System.out.println("drop columnfamily. ks:" + ksName + ", cf:" + cfName);
         
         cluster.dropKeyspace(ksName);
         System.out.println("drop keyspace:" + ksName);
         
+    }
+    
+    private static void deleteRows(Cluster cluster, String ksName, String cfName, List<String> rowKeys) {
+        Keyspace ksp = HFactory.createKeyspace(ksName, cluster);
+        ColumnFamilyTemplate<String, String> template = new ThriftColumnFamilyTemplate<String, String>(ksp, cfName, StringSerializer.get(), StringSerializer.get());
+        Mutator<String> m = template.createMutator();
+        m.addDeletion(rowKeys, cfName);
+        m.execute();
+        
+        if ( !readRows(cluster, ksName, cfName, rowKeys).isEmpty() ) {
+            throw new RuntimeException("shouldn't find rows by keys:" + rowKeys);
+        }
+    }
+    
+    private static Map<String, Map<String, String>> readRows(Cluster cluster, String ksName, String cfName, List<String> rowKeys) {
+        Keyspace ksp = HFactory.createKeyspace(ksName, cluster);
+        ColumnFamilyTemplate<String, String> template = new ThriftColumnFamilyTemplate<String, String>(ksp, cfName, StringSerializer.get(), StringSerializer.get());
+        ColumnFamilyResult<String, String> results = template.queryColumns(rowKeys);
+        Map<String,Map<String,String>> result = new LinkedHashMap<>();
+        while (results.hasResults()) {
+            Map<String,String> row = new LinkedHashMap<>();
+            for ( String columnName: results.getColumnNames() ) {
+                row.put(columnName, results.getString(columnName));
+            }
+            result.put(results.getKey(), row);
+            if (results.hasNext()) {
+                results = results.next();
+            } else {
+                break;
+            }
+        }
+        return result;
     }
     
     private static void deleteRow(Cluster cluster, String ksName, String cfName, String rowKey) {
@@ -66,7 +111,6 @@ public class Thrift {
         if ( !readRow(cluster, ksName, cfName, rowKey).isEmpty() ) {
             throw new RuntimeException("shouldn't find row by key:" + rowKey);
         }
-        
     }
     
     private static Map<String, String> readRow(Cluster cluster, String ksName, String cfName, String rowKey) {
@@ -80,12 +124,30 @@ public class Thrift {
         return result;
     }
     
+    private static List<String> createRows(Cluster cluster, String ksName, String cfName) {
+        List<String> rowKeys = new ArrayList<>();
+        for ( int i = 0; i < 3; i++ ) {
+            rowKeys.add("rowKey_" + i);
+        }
+        Keyspace ksp = HFactory.createKeyspace(ksName, cluster);
+        ColumnFamilyTemplate<String, String> template = new ThriftColumnFamilyTemplate<String, String>(ksp, cfName, StringSerializer.get(), StringSerializer.get());
+        ColumnFamilyUpdater<String, String> updater = template.createUpdater();
+        for ( String rowKey: rowKeys ) {
+            updater.addKey(rowKey);
+            for ( int i = 0; i < 3; i++ ) {
+                updater.setString(rowKey + "_column_" + i, "test" + i);
+            }
+        }
+        template.update(updater);
+        return rowKeys;
+    }
+    
     private static String createRow(Cluster cluster, String ksName, String cfName) {
         String rowKey = "rowKey_" + new Random().nextInt(100);
         Keyspace ksp = HFactory.createKeyspace(ksName, cluster);
         ColumnFamilyTemplate<String, String> template = new ThriftColumnFamilyTemplate<String, String>(ksp, cfName, StringSerializer.get(), StringSerializer.get());
         ColumnFamilyUpdater<String, String> updater = template.createUpdater(rowKey);
-        for ( int i = 0; i < 10; i++ ) {
+        for ( int i = 0; i < 3; i++ ) {
             updater.setString("test" + i, "test" + i);
         }
         template.update(updater);
