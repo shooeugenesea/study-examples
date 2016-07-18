8cffeb4b7e809a4e7c68c44068605a69a20811d6
shooeugenesea.tw <shooeugenesea.tw@yahoo.com.tw>
2016-07-19 01:21:50 +0800

    add test

--- src/test/java/guava/OrderingTest.java
+++ src/test/java/guava/OrderingTest.java
@@ -1,23 +1,43 @@
 package guava;
 
 import com.google.common.collect.ComparisonChain;
+import com.google.common.collect.Ordering;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.*;
 
 public class OrderingTest {
 
+    private final NumbersObject n1 = new NumbersObject(1, 2, 3);
+    private final NumbersObject n2 = new NumbersObject(1, 2, 4);
+    private final NumbersObject n3 = new NumbersObject(-1, 2, -3);
+    private final NumbersObject n4 = new NumbersObject(-2, 3, 5);
+    private List<NumbersObject> numbers;
+
+    @Before
+    public void before() {
+        numbers = Arrays.asList(n1, n2, n3, n4);
+    }
+
     @Test
-    public void natural() {
-        List<NumbersObject> numbers = Arrays.asList(
-                new NumbersObject(1,2,3),
-                new NumbersObject(1,2,4),
-                new NumbersObject(-1,2,-3),
-                new NumbersObject(-2,3,5)
-        );
-        System.out.println(numbers);
-        Collections.sort(numbers, );
+    public void usingToString() {
+        Collections.sort(numbers, Ordering.usingToString());
+        Assert.assertEquals(n3, numbers.get(0));
+        Assert.assertEquals(n4, numbers.get(1));
+        Assert.assertEquals(n1, numbers.get(2));
+        Assert.assertEquals(n2, numbers.get(3));
+    }
+
+    @Test
+    public void reverse() {
+//        Collections.sort(numbers, new Ordering<NumbersObject>() {
+//            @Override
+//            public int compare(NumbersObject numbersObject, NumbersObject t1) {
+//
+//            }
+//        });
     }
 
 }
