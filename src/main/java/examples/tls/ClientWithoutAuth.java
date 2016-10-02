60dc77b96ba1d62c74927268a9d095faa81d830c
shooeugenesea.tw <shooeugenesea.tw@yahoo.com.tw>
2016-10-03 00:46:05 +0800

    move codes to main method

--- src/main/java/examples/tls/ClientWithoutAuth.java
+++ src/main/java/examples/tls/ClientWithoutAuth.java
@@ -1,16 +1,12 @@
 package examples.tls;
 
 import javax.net.SocketFactory;
-import javax.net.ssl.KeyManagerFactory;
-import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSocketFactory;
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.nio.file.Paths;
-import java.security.KeyStore;
 
 /**
  * Reference http://www.programgo.com/article/71463377839/
@@ -19,42 +15,16 @@ import java.security.KeyStore;
 public class ClientWithoutAuth {
 
     private static String CLIENT_KEY_STORE = Paths.get("src/main/resources/certs/catrust.jks").toAbsolutePath().toString();
-    private static String CLIENT_KEY_STORE_PASSWORD = "123456";
 
     public static void main(String[] params) throws Exception {
-        connect();
-    }
-
-    private static Socket createNonAuthenticationSocket()throws Exception{
         System.setProperty("javax.net.ssl.trustStore", CLIENT_KEY_STORE);
         SocketFactory sf = SSLSocketFactory.getDefault();
         Socket s = sf.createSocket("localhost", 8443);
-        return s;
-    }
-
-    private static Socket createAuthenticationSocket() throws Exception{
-        System.setProperty("javax.net.ssl.trustStore", CLIENT_KEY_STORE);
-        SSLContext context = SSLContext.getInstance("TLS");
-        KeyStore ks = KeyStore.getInstance("jceks");
-        ks.load(new FileInputStream(CLIENT_KEY_STORE), null);
-        KeyManagerFactory kf = KeyManagerFactory.getInstance("SunX509");
-        kf.init(ks, CLIENT_KEY_STORE_PASSWORD.toCharArray());
-        context.init(kf.getKeyManagers(), null, null);
-
-        SocketFactory factory = context.getSocketFactory();
-        Socket s = factory.createSocket("localhost", 8443);
-        return s;
-    }
-
-    private static void connect()throws Exception{
-        Socket s = createNonAuthenticationSocket();
-//     Socket s = createAuthenticationSocket();
-
         PrintWriter writer = new PrintWriter(s.getOutputStream());
         BufferedReader reader = new BufferedReader(new InputStreamReader(s.getInputStream()));
-        writer.println("hello");
+        writer.println("hello\n");
         writer.flush();
-        System.out.println(reader.readLine());
+        System.out.println("client receive:" + reader.readLine());
         s.close();
     }
 
--- src/main/java/examples/tls/ServerDontNeedClientAuth.java
+++ src/main/java/examples/tls/ServerDontNeedClientAuth.java
@@ -1,13 +1,17 @@
 package examples.tls;
 
 import javax.net.ServerSocketFactory;
-import javax.net.ssl.*;
-import java.io.*;
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLServerSocket;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.file.Paths;
 import java.security.KeyStore;
-import java.util.logging.Logger;
 
 /**
  * Reference http://www.programgo.com/article/71463377839/
@@ -19,37 +23,9 @@ public class ServerDontNeedClientAuth {
     private static String SERVER_KEY_STORE_PASSWORD = "123456";
 
     public static void main(String[] params) throws Exception {
-        SSLServerSocket sslServerSocket= createSSLServerSocket();
-
-        while(true){
-            try{
-                System.out.println("listen port 8443..");
-                Socket socket = sslServerSocket.accept();
-                System.out.println("accept:" + socket);
-                InputStream is = socket.getInputStream();
-                byte[] bytes = new byte[Short.MAX_VALUE];
-                int len = -1;
-
-                while((len = is.read(bytes))>0){
-                    System.out.println(new String(bytes,0,len));
-                    if(len<bytes.length){
-                        break;
-                    }
-                }
-                socket.getOutputStream().write("1234567890".getBytes());
-                socket.close();
-            }catch(Exception e){
-                e.printStackTrace();
-            }
-        }
-    }
-
-    private static SSLServerSocket createSSLServerSocket() throws Exception{
-        // whether enable the debug mode
-        System.setProperty("javax.net.debug", "ssl,handshake");
+//        System.setProperty("javax.net.debug", "ssl,handshake");
         System.setProperty("javax.net.ssl.trustStore", SERVER_KEY_STORE);
         SSLContext context = SSLContext.getInstance("TLS");
-
         KeyStore ks = KeyStore.getInstance("jceks");
         ks.load(new FileInputStream(SERVER_KEY_STORE), null);
         KeyManagerFactory kf = KeyManagerFactory.getInstance("SunX509");
@@ -59,9 +35,22 @@ public class ServerDontNeedClientAuth {
         ServerSocketFactory factory = context.getServerSocketFactory();
         ServerSocket serverSocket = factory.createServerSocket(8443);
         SSLServerSocket sslServerSocket =  (SSLServerSocket) serverSocket;
-        // set whether need the client authentication
         sslServerSocket.setNeedClientAuth(false);
-        return sslServerSocket;
+        while(true){
+            try{
+                System.out.println("listen port 8443..");
+                Socket socket = sslServerSocket.accept();
+                System.out.println("accept:" + socket);
+                InputStream is = socket.getInputStream();
+                BufferedReader buffer = new BufferedReader(new InputStreamReader(is));
+                String readLine = buffer.readLine();
+                System.out.println("server receive:" + readLine);
+                socket.getOutputStream().write("1234567890".getBytes());
+                socket.close();
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
     }
 
 }
