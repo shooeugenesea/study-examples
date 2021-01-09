4fe64286ca849183e1e6c90ad6060c27df34bcc1
Isaac Liao <shooeugenesea.tw@gmail.com>
2021-01-10 01:52:08 +0800

    Add Kafka Producer and Consumer simple example

--- src/main/java/examples/Main.java
+++ src/main/java/examples/Main.java
@@ -2,46 +2,111 @@ package examples;
 
 import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.consumer.*;
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
+import java.time.Duration;
+import java.util.*;
+import java.util.concurrent.*;
 
 public class Main {
 
+    private static final ExecutorService e = Executors.newCachedThreadPool();
+
     public static void main(String[] args) throws InterruptedException, ExecutionException, TimeoutException {
-        Properties kafkaProps = kafkaProps("dev:9092");
-        createTopicIfNotExist(kafkaProps, "mytopic");
-
-
-//        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(kafkaProps);
-//
-//        ProducerRecord<String, String> record = new ProducerRecord<>("mytopic", "myval");
-//        try {
-//            producer.send(record);
-//        } catch (Throwable ex) {
-//            ex.printStackTrace();
-//        }
-
+        String topic = "mytopic";
+        Properties kafkaProps = kafkaProps("localhost:9092");
+        createTopicIfNotExist(kafkaProps, topic);
+        keepSendingMessage(kafkaProps, topic);
+        keepConsumingMessage(kafkaProps, topic);
     }
+
+    private static void keepConsumingMessage(Properties kafkaProps, String topic) {
+        e.execute(() -> {
+            try (KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(kafkaProps)) {
+                consumer.subscribe(Arrays.asList(topic));
+                ConsumerRecords<String, String> records = null;
+                while (true) {
+                    records = consumer.poll(Duration.ofSeconds(10));
+                    if (records.count() > 0) {
+                        System.out.println("receive:" + toString(records));
+                    }
+                }
+            } catch (Throwable ex) {
+                ex.printStackTrace();
+            }
+        });
+    }
+
+    private static void keepSendingMessage(Properties kafkaProps, String topic) {
+        e.execute(() -> {
+            int count = 0;
+            while (count++ < 10000) {
+                try (KafkaProducer<String, String> producer = new KafkaProducer<String, String>(kafkaProps)) {
+                    ProducerRecord<String, String> record = new ProducerRecord<>(topic, "mykey", "myval" + count);
+                    System.out.println("offset:" + producer.send(record).get(10, TimeUnit.SECONDS).offset());
+                } catch (Throwable ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+    }
+
+
+    public static void main2(String[] args) throws InterruptedException, ExecutionException, TimeoutException {
+        String topic = "mytopic";
+        Properties kafkaProps = kafkaProps("localhost:9092");
+        createTopicIfNotExist(kafkaProps, topic);
+
+        try (KafkaProducer<String, String> producer = new KafkaProducer<String, String>(kafkaProps);
+             KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(kafkaProps);
+        ) {
+            consumer.subscribe(Arrays.asList(topic));
+            ProducerRecord<String, String> record = new ProducerRecord<>("mytopic", "mykey", "myval");
+            System.out.println("offset:" + producer.send(record).get(10, TimeUnit.SECONDS).offset());
+            int retry = 0;
+            ConsumerRecords<String, String> records = null;
+            while (retry++ < 10) {
+                records = consumer.poll(Duration.ofSeconds(10));
+                if (records.count() > 0) {
+                    break;
+                }
+            }
+            System.out.println("receive:" + toString(records));
+        } catch (Throwable ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private static String toString(ConsumerRecords<String, String> records) {
+        System.out.println("count:" + records.count());
+        StringBuilder sb = new StringBuilder();
+        for (ConsumerRecord<String, String> record : records) {
+            sb.append(record.toString());
+        }
+        return sb.toString();
+    }
+
 
     private static Properties kafkaProps(String url) {
         Properties properties = new Properties();
-        properties.put("bootstrap.servers", url);
-        properties.put("key.serializer", StringSerializer.class.getName());
-        properties.put("value.serializer", StringSerializer.class.getName());
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, url);
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "TestGroup");
+
         return properties;
     }
 
     private static void createTopicIfNotExist(Properties kafkaProps, String topic) throws InterruptedException, ExecutionException, TimeoutException {
         AdminClient adminClient = AdminClient.create(kafkaProps);
-        NewTopic newTopic = new NewTopic("topicName", 1, /* replicationFactor */ (short)1);
+        NewTopic newTopic = new NewTopic(topic, 1, /* replicationFactor */ (short)1);
         if ( adminClient.listTopics().names().get(10, TimeUnit.SECONDS).contains(topic) ) {
             System.out.println("Topic " + topic + " already exist");
             return;
